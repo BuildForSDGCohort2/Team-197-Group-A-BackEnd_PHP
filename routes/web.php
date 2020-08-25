e8c5c19a1d8190ca853368beff6550b7a0cd6ebf
obizke <mauriq97@gmail.com>
2020-08-25 18:21:25 +0300

    api-generator

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->middleware('verified');
+
+Route::resource('users', 'UserController');
